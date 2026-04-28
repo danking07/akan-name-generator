2fd4e9b797f2042a9ad3af5501f54d6fbd2775b8
Dan <danking9397@gmail.com>
2026-04-28 08:48:56 +0300

    Add navigation links in index.html and adjust styles for result display in styles.css

---
+++ script.js
@@ -0,0 +1,11 @@
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+const maleNames  = ["Kwasi", "Kwado", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+const femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+
+
+function getDayOfWeek(day, month, year) {
+    if (month <= 2){
+        month = month + 12;
+    }
+}
